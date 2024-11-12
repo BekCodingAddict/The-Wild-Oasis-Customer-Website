81f5488293e5123f03cec15120fbcbed14c5c0f2
unknown <experte001@gmail.com>
2024-11-13 01:35:50 +0900

    Advanced:Server Components in Client Components

--- client/app/cabins/page.js
+++ client/app/cabins/page.js
@@ -3,13 +3,15 @@ import CabinList from "../_components/CabinList";
 import { getCabins } from "../_lib/data-service";
 import CabinCard from "./CabinCard";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 export const metadata = {
   title: "Cabins",
 };
 
 export const revalidate = 3600;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +25,12 @@ export default function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
