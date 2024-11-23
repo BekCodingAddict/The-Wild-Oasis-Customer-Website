4e91f52ebd36601bc85873d93a1b2c81ce96ce01
unknown <experte001@gmail.com>
2024-11-24 02:46:42 +0900

    Creating an API Endpoint width Route Handler

--- client/app/_components/Counter.js
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useState } from "react";
-
-function Counter({ users }) {
-  const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>{count}</button>;
-}
-
-export default Counter;
